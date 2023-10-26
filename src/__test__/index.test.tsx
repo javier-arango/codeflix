859d312debdf16cd858181d5ee51b798c1b3c800
Javier Arango <javierarango.developer@gmail.com>
2023-10-26 00:20:05 -0400

    added husky a precommit check and jest for testing

---
+++ src/__test__/index.test.tsx
@@ -0,0 +1,15 @@
+import { render, screen } from '@testing-library/react'
+import HomePage from '../app/page'
+import '@testing-library/jest-dom'
+ 
+describe('HomePage', () => {
+  it('renders a heading', () => {
+    render(<HomePage />)
+ 
+    const heading = screen.getByRole('heading', {
+      name: /Cinemify/i,
+    })
+ 
+    expect(heading).toBeInTheDocument()
+  })
+})
