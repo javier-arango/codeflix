c8534600d88f4d5f44646ef6ee87dc86c4b4f0d5
Jean Fonrose <21975726+jeangregorfonrose@users.noreply.github.com>
2023-11-08 21:51:32 -0500

    Created the Auth component

---
+++ src/components/Auth.tsx
@@ -0,0 +1,10 @@
+export default function Auth () {
+    return (
+      <section id="authentication">
+        <div className="container">
+          <div id="left"></div>
+          <div id="right"></div>
+        </div>
+      </section>
+    );
+}
