87a2550e37d3886d1eb3b3e159c89acb3301176e
Jean Fonrose <21975726+jeangregorfonrose@users.noreply.github.com>
2023-11-08 21:49:18 -0500

    Changed font in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,13 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Roboto } from 'next/font/google'
 import '@styles/globals.scss'
 
-const inter = Inter({ subsets: ['latin'] })
+const roboto = Roboto({weight: "400", style: "normal", subsets: ["latin"]});
 
 export const metadata: Metadata = {
-  title: 'Cinemify',
+  title: 'Codeflix',
   description:
-    'Cinemify aims to be the ultimate destination for movie enthusiasts, providing a comprehensive and interactive platform to explore, review, and watch films.',
+    'Codeflix aims to be the ultimate destination for movie enthusiasts, providing a comprehensive and interactive platform to explore, review, and watch films.',
 }
 
 export default function RootLayout({
@@ -17,7 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={roboto.className}>{children}</body>
     </html>
   )
 }
