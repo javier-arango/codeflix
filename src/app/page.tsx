6dbc772a8e49a09bf36695d84f692797a1e3d04e
Jean Fonrose <21975726+jeangregorfonrose@users.noreply.github.com>
2023-11-08 20:33:30 -0500

    Added code for Banner component, and created assets folder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import Banner from "@components/Banner"
 import NavBar from "@components/NavBar"
 
 export default function HomePage() {
   return (
     <main>
       <NavBar />
+      <Banner />
     </main>
   )
 }
--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,3 +1,20 @@
-export default function Banner () {
-    
+import styles from '../styles/Banner.module.scss';
+
+export default function Banner() {
+  return (
+    <section id={styles.banner}>
+      <div id={styles.container} className="container">
+        <div id={styles.imgCont}>
+          <img id={styles.image} src="learning.svg" alt="My Happy SVG" />
+        </div>
+        <div id={styles.msgCont}>
+          <h1 id={styles.title}>Level up your skills</h1>
+          <p>
+            Get the knowledge, experience, and confidence you need to <br />
+            make you get better in your computer science career.
+          </p>
+        </div>
+      </div>
+    </section>
+  )
 }
