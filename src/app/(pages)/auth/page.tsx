93e7c75b12b1f8d422fdafe515243c3b5b1d255d
Jean Fonrose <21975726+jeangregorfonrose@users.noreply.github.com>
2023-11-08 21:42:05 -0500

    Created auth page

---
+++ src/app/(pages)/auth/page.tsx
@@ -0,0 +1,12 @@
+import NavBar from '@components/NavBar'
+
+export default function AuthPage() {
+  return (
+    <main>
+      <NavBar />
+    </main>
+  )
+}
+
+// Display name
+AuthPage.displayName = 'Authentication'
