dcd671ffcb6d29298a7501ac3d4a8c370c45bcff
Jean Fonrose <21975726+jeangregorfonrose@users.noreply.github.com>
2023-11-08 20:19:01 -0500

    started witht the design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,9 @@
-import { getServerSession } from 'next-auth'
-import { authOptions } from './api/auth/[...nextauth]/route'
 import NavBar from '@components/NavBar'
 
-export default async function HomePage() {
-  const session = await getServerSession(authOptions)
-
+export default function HomePage() {
   return (
     <main>
-      {/* <NavBar userName={JSON.stringify(session?.user?.name)} /> */}
       <NavBar />
-
-      <h2>Server Session</h2>
-      <pre>{JSON.stringify(session)}</pre>
-      <h2>Client Call</h2>
     </main>
   )
 }
