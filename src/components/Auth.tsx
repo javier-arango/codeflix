229a5693f13054fb88bf640bb2f7a188cbf7718e
Jean Fonrose <21975726+jeangregorfonrose@users.noreply.github.com>
2023-11-10 20:26:21 -0500

    Added graphics and message to auth section

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -1,10 +1,17 @@
+import Image from 'next/image';
+import styles from '../styles/Auth.module.scss';
+import authImage from '../../public/assets/test-online.svg';
+
 export default function Auth () {
     return (
-      <section id="authentication">
-        <div className="container">
-          <div id="left"></div>
-          <div id="right"></div>
+      <section id={styles.auth}>
+        <div id={styles.container} className="container">
+          <div id={styles.messageCont}>
+            <h1 id={styles.message}>Message</h1>
+            <Image id={styles.image} src={authImage} alt="Learning picture" />
+          </div>
+          <div id={styles.authCont}></div>
         </div>
       </section>
-    );
+    )
 }
