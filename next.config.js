b98f95fac04a8eeaef55128785774d546c7a3c95
Javier Arango <javierarango.developer@gmail.com>
2023-11-14 13:26:02 -0500

    fixing eslint errors

--- next.config.js
+++ next.config.js
@@ -7,16 +7,6 @@ const nextConfig = {
     includePaths: [path.join(__dirname, 'src', 'styles')],
     prependData: `@import "main.scss";`,
   },
-  // Only accepts images from image.tmdb.org
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'image.tmdb.org',
-        pathname: '/t/p/**',
-      },
-    ],
-  },
 }
 
 module.exports = nextConfig
