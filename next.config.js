c5be5f85dd58d921944c913419f46b8daf4ef8c3
Javier Arango <javierarango.developer@gmail.com>
2023-11-15 18:30:19 -0500

    major/switch to tailwing

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
-const path = require('path')
 
 const nextConfig = {
-  // Sass configuration
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'src', 'styles')],
-    prependData: `@import "main.scss";`,
-  },
   images: {
     remotePatterns: [
       {
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import '@styles/globals.css'
 import type { Metadata } from 'next'
 import { Roboto } from 'next/font/google'
 import { Toaster } from 'react-hot-toast'
