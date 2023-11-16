f6ca05f59dcfbf0c29aded7e9f1b0babae3bcbd9
Jean Fonrose <21975726+jeangregorfonrose@users.noreply.github.com>
2023-11-16 02:01:57 -0300

    Created the category page

---
+++ src/app/(pages)/categories/[category]/page.tsx
@@ -0,0 +1,11 @@
+import NavBar from '@components/NavBar'
+import Title from '@components/Title'
+
+export default function CategoriesPage() {
+  return (
+    <>
+      <NavBar />
+      <Title title={'Browse by Categories'} />
+    </>
+  )
+}
