c6f80f641ce477badc4546be569cc24fb9487e6f
Jean Fonrose <21975726+jeangregorfonrose@users.noreply.github.com>
2023-11-16 20:08:35 -0300

    Fixed categories page

--- src/app/(pages)/categories/page.tsx
+++ src/app/(pages)/categories/page.tsx
@@ -1,13 +1,30 @@
 import Categories from '@components/Categories'
 import NavBar from '@components/NavBar'
 import Title from '@components/Title'
+import { CategoryInfo, CategoryKey, VIDEO_CATEGORIES2 } from '@constants/videoCategories.constants';
 
 export default function CategoriesPage() {
+
+  const getCategories = function (
+  ): CategoryInfo[] {
+    let categories = [];
+
+    for (const key in VIDEO_CATEGORIES2) {
+      if (VIDEO_CATEGORIES2.hasOwnProperty(key)) {
+        const categoryKey: CategoryKey = key as CategoryKey
+        const categoryInfo: CategoryInfo = VIDEO_CATEGORIES2[categoryKey]
+        categories.push(categoryInfo)
+      }
+    }
+
+    return categories
+  }
+
   return (
     <>
       <NavBar />
       <Title title={'Browse by Categories'} />
-      <Categories allCategories={true} categories={[]} />
+      <Categories allCategories={true} categories={getCategories()} />
     </>
   )
 }
