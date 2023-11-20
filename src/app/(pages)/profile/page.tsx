0093899257d6c483f490053afcb571f4ff16da5f
Jean Fonrose <21975726+jeangregorfonrose@users.noreply.github.com>
2023-11-20 18:31:25 -0500

    Modified Profile component and added it to profile page

--- src/app/(pages)/profile/page.tsx
+++ src/app/(pages)/profile/page.tsx
@@ -1,9 +1,11 @@
 import NavBar from '@components/NavBar'
+import Profile from '@components/Profile'
 
 export default function ProfilePage() {
   return (
     <main>
       <NavBar />
+      <Profile />
     </main>
   )
 }
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,17 +1,19 @@
+import styles from '@styles/Profile.module.scss'
+import Tabs from './Tabs'
+import Tab from './Tab'
+
 export default function Profile () {
   return (
-    <section id="profile">
-      <div className="container">
-        <div id="pic-and-name">
-          <div id="pic"></div>
-          <h1 id="name">John Peter</h1>
+    <section id={styles.profile}>
+      <div id={styles.container} className="container">
+        <div id={styles.picAndName}>
+          <div id={styles.pic}></div>
+          <h1 id={styles.name}>John Peter</h1>
         </div>
-        <div id="tab">
-          <ul>
-            <li className="active">Watchlist</li>
-            <li>Favorites</li>
-          </ul>
-        </div>
+        <Tabs labels={["watchlist", "favorite"]}>
+          <Tab label="Watchlist" content={<p>Content for Tab 1 goes here.</p>} />
+          <Tab label="Favorite" content={<p>Content for Tab 2 goes here.</p>} />
+        </Tabs>
       </div>
     </section>
   )
