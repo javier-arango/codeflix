4d0ee5dc7c24a8516cc9ebed7488c2af73dd5555
Jean Fonrose <21975726+jeangregorfonrose@users.noreply.github.com>
2023-11-20 18:30:07 -0500

    Created tabs component

---
+++ src/components/Tabs.tsx
@@ -0,0 +1,38 @@
+'use client'
+
+import styles from '@styles/Tabs.module.scss'
+import React, { useState } from 'react'
+
+type Props = {
+  children: JSX.Element[]
+  labels: string[]
+}
+
+export default function Tabs({ children, labels } : Props) {
+  const [activeTab, setActiveTab] = useState(0)
+
+  const handleTabClick = (index: number) => {
+    setActiveTab(index)
+  }
+
+
+  return (
+    <div id={styles.tab}>
+      <div id={styles.tabHeader}>
+        <ul id={styles.tabOptions}>
+          {React.Children.map(children, (child, index: number) => (
+            <li
+              className={`${styles.tabOption} ${
+                index === activeTab && styles.active
+              }`}
+              onClick={() => handleTabClick(index)}
+            >
+              {labels[index]}
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div>{children[activeTab]}</div>
+    </div>
+  )
+}
