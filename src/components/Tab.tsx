99d401131237a949ee9b30943ec7a3e6c723c113
Jean Fonrose <21975726+jeangregorfonrose@users.noreply.github.com>
2023-11-20 20:28:04 -0500

    Fixed tab props type

--- src/components/Tab.tsx
+++ src/components/Tab.tsx
@@ -1,4 +1,9 @@
-export default function Tab({ label, content }) {
+type Props = {
+  label: string
+  content: React.JSX.Element
+}
+
+export default function Tab({ label, content }: Props) {
   return (
     <div>
       <h3>{label}</h3>
