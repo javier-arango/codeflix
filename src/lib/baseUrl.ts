8088bb7d839a0fec4b9dcc85bb55b108c8e2623a
Javier Arango <javierarango.developer@gmail.com>
2023-11-21 21:05:14 -0500

    feature/getting base url

--- src/lib/baseUrl.ts
+++ src/lib/baseUrl.ts
@@ -1,17 +1,11 @@
 import { getEnvVariable } from '@utils/index'
 
 const getBaseURL = () => {
-  const vercelENV = getEnvVariable('NEXT_PUBLIC_VERCEL_ENV')
-
   if (getEnvVariable('NODE_ENV') === 'development') {
     return getEnvVariable('NEXT_PUBLIC_URL')
-  } else {
-    if (vercelENV === 'preview') {
-      return `https://${getEnvVariable('NEXT_PUBLIC_VERCEL_BRANCH_URL')}`
-    }
   }
 
-  return `https://${getEnvVariable('NEXT_PUBLIC_VERCEL_URL')}`
+  return `https://${getEnvVariable('VERCEL_URL')}`
 }
 
 // Base URL for API calls
