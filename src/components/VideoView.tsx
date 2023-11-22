649a3c15d8125dc8862bdc67fbc86930072c4289
Jean Fonrose <21975726+jeangregorfonrose@users.noreply.github.com>
2023-11-22 17:53:33 -0500

    Added icons for actions in video view

--- src/components/VideoView.tsx
+++ src/components/VideoView.tsx
@@ -3,7 +3,10 @@
 import type { Video } from '@prisma/client'
 import styles from '@styles/VideoView.module.scss'
 import { fetcher } from '@utils/fetcher.utils'
+import Image from 'next/image'
 import useSWR from 'swr'
+import bookmark from '../../public/assets/bookmark.svg'
+import star from '../../public/assets/star.svg'
 import { LoadingSpinner } from './LoadingSpinner'
 import { VideoPlayer } from './VideoPlayer'
 
@@ -35,7 +38,18 @@ export default function VideoView({ videoId }: VideoViewProps) {
           </div>
           <div id={styles.infoActions}>
             <h1 id={styles.title}>{data?.title}</h1>
-            <div id={styles.action}></div>
+            <div id={styles.action}>
+              <Image
+                className={styles.actionsIcons}
+                src={star}
+                alt="bookmark icon"
+              />
+              <Image
+                className={styles.actionsIcons}
+                src={bookmark}
+                alt="bookmark icon"
+              />
+            </div>
           </div>
         </div>
         <div id={styles.videosList}></div>
