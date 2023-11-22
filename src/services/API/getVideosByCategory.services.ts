5b7364a1471ccd085878eaf5d936bab4709dff5a
Javier Arango <javierarango.developer@gmail.com>
2023-11-22 14:22:56 -0500

    feature/create services to fetch the backend api

---
+++ src/services/API/getVideosByCategory.services.ts
@@ -0,0 +1,26 @@
+import type { CategoryKey } from '@constants/index'
+import { baseURL } from '@lib/index'
+import type { VideoListResponse } from 'types'
+
+export async function GetVideosByCategory(
+  categoryId: CategoryKey
+): Promise<VideoListResponse> {
+  try {
+    const res = await fetch(`${baseURL}/api/videos?category=${categoryId}`)
+
+    if (!res.ok) {
+      return {
+        count: 0,
+        videos: [],
+      }
+    }
+
+    return await res.json()
+  } catch (err) {
+    console.error(err)
+    return {
+      count: 0,
+      videos: [],
+    }
+  }
+}
---
+++ src/services/API/searchVideos.services.ts
@@ -0,0 +1,26 @@
+import { baseURL } from '@lib/index'
+import type { SearchResponse } from 'types'
+
+export async function SearchVideos(query?: string): Promise<SearchResponse> {
+  try {
+    const res = await fetch(`${baseURL}/api/search`, {
+      method: 'POST',
+      body: JSON.stringify({ query }),
+    })
+
+    if (!res.ok) {
+      return {
+        count: 0,
+        result: [],
+      }
+    }
+
+    return await res.json()
+  } catch (err) {
+    console.error(err)
+    return {
+      count: 0,
+      result: [],
+    }
+  }
+}
