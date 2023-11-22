818f80c7b74f7e007dc1bd1e123cae8dd92ec0b0
Javier Arango <javierarango.developer@gmail.com>
2023-11-21 20:09:11 -0500

    feature/getting base url

--- src/lib/baseUrl.ts
+++ src/lib/baseUrl.ts
@@ -5,10 +5,11 @@ const env = getEnvVariable('NEXT_PUBLIC_VERCEL_ENV')
 
 // Vercel
 const isProduction =
-  env === 'production' && getEnvVariable('NEXT_PUBLIC_VERCEL_URL')
+  env === 'production' && `https://${getEnvVariable('NEXT_PUBLIC_VERCEL_URL')}`
 
 const isPreview =
-  env === 'preview' && getEnvVariable('NEXT_PUBLIC_VERCEL_BRANCH_URL')
+  env === 'preview' &&
+  `https://${getEnvVariable('NEXT_PUBLIC_VERCEL_BRANCH_URL')}`
 
 const isDevelopment =
   env === 'development' && getEnvVariable('NEXT_PRIVATE_URL')
