98bc6bca7b450119239d78a85463a52f78a6510f
Javier Arango <javierarango.developer@gmail.com>
2023-11-21 20:41:49 -0500

    feature/getting base url

--- src/lib/baseUrl.ts
+++ src/lib/baseUrl.ts
@@ -1,18 +1,12 @@
 import { getEnvVariable } from '@utils/index'
 
-// Get env variable
-const env = getEnvVariable('NEXT_PUBLIC_VERCEL_ENV')
+const getBaseURL = () => {
+  if (getEnvVariable('NODE_ENV') === 'development') {
+    return getEnvVariable('NEXT_PUBLIC_URL')
+  }
 
-// Vercel
-const isProduction =
-  env === 'production' && `https://${getEnvVariable('NEXT_PUBLIC_VERCEL_URL')}`
+  return `https://${getEnvVariable('NEXT_PUBLIC_VERCEL_URL')}`
+}
 
-const isPreview =
-  env === 'preview' &&
-  `https://${getEnvVariable('NEXT_PUBLIC_VERCEL_BRANCH_URL')}`
-
-const isDevelopment =
-  env === 'development' && getEnvVariable('NEXT_PRIVATE_URL')
-
-// BAse URL
-export const baseURL = isProduction || isPreview || isDevelopment
+// Base URL for API calls
+export const baseURL = getBaseURL()
