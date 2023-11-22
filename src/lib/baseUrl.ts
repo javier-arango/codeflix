8870fe97cc8a7a1ba4156e3d53378fd0449b9455
Javier Arango <javierarango.developer@gmail.com>
2023-11-21 19:48:33 -0500

    feature/getting base url

--- src/lib/baseUrl.ts
+++ src/lib/baseUrl.ts
@@ -1,3 +1,6 @@
 import { getEnvVariable } from '@utils/index'
 
-export const baseURL = getEnvVariable('NEXT_PUBLIC_API_URL')
+export const baseURL = getEnvVariable(
+  'NEXT_PUBLIC_API_URL' || 'NEXT_PUBLIC_VERCEL_BRANCH_URL'
+)
+console.log('baseURL', baseURL)
