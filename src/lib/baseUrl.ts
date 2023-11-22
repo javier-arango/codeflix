b162d3cd0c93495ce6a45ff2e97172990580c9da
Javier Arango <javierarango.developer@gmail.com>
2023-11-21 20:52:12 -0500

    feature/getting base url

--- src/lib/baseUrl.ts
+++ src/lib/baseUrl.ts
@@ -1,8 +1,14 @@
 import { getEnvVariable } from '@utils/index'
 
 const getBaseURL = () => {
+  const vercelENV = getEnvVariable('NEXT_PUBLIC_VERCEL_ENV')
+
   if (getEnvVariable('NODE_ENV') === 'development') {
     return getEnvVariable('NEXT_PUBLIC_URL')
+  } else {
+    if (vercelENV === 'preview') {
+      return `https://${getEnvVariable('NEXT_PUBLIC_VERCEL_BRANCH_URL')}`
+    }
   }
 
   return `https://${getEnvVariable('NEXT_PUBLIC_VERCEL_URL')}`
