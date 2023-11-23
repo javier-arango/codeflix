fe1d4e203538539092a9913aff74c595082a721e
Jean Fonrose <21975726+jeangregorfonrose@users.noreply.github.com>
2023-11-23 12:21:03 -0500

    Updated types to fix deployment failed

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,6 @@
 // Export everything from the types folder here
 
-import type { Video } from '@prisma/client'
+import type { Playlist, Video } from '@prisma/client'
 import type { StaticImageData } from 'next/image'
 
 export type CategoryKey =
@@ -28,4 +28,25 @@ export type CategoryInfo = {
   key: CategoryKey
   name: string
   img: StaticImageData
+}
+
+export interface PlaylistListResponse {
+  count: number
+  playlists: Playlist[]
+}
+
+export interface UpdatedProfile {
+  firstName?: string
+  lastName?: string
+  avatar?: string
+  bio?: string
+}
+
+export interface CreatePlaylist {
+  userEmail: string
+  videoId?: string
+  playlist: {
+    name: string
+    description?: string
+  }
 }
