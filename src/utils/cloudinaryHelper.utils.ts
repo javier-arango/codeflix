4400dfd41e5354b2564e28111a78c9023448b26d
Javier Arango <javierarango.developer@gmail.com>
2023-12-05 12:49:08 -0500

    feature/services to upload and delete profile picture

---
+++ src/utils/cloudinaryHelper.utils.ts
@@ -0,0 +1,24 @@
+import crypto from 'crypto'
+
+export function getCloudinaryPublicIdFromUrl(url: string): string | null {
+  const regex = /\/upload\/(?:v\d+\/)?(.+?)\.[^.]+$/
+  const match = url.match(regex)
+
+  return match ? match[1] : null
+}
+
+export function generateSHA1ForCloudinary(data: string) {
+  const hash = crypto.createHash('sha1')
+  hash.update(data)
+
+  return hash.digest('hex')
+}
+
+export function generateSignatureForCloudinary(
+  publicId: string,
+  apiSecret: string
+) {
+  const timestamp = new Date().getTime()
+
+  return `public_id=${publicId}&timestamp=${timestamp}${apiSecret}`
+}
